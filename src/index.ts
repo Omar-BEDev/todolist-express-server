6ea497acef0d4a17fc1a23e9082deac2c67e3ad3
Omar <kaliochibrahim@gmail.com>
2026-01-30 19:47:16 +0100

    Configure CORS middleware with specific options

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,11 @@ import swaggerDocument from "./docs/swagger.json"
 
 
 const app = express();
-app.use(cors())
+app.use(cors({
+  origin: '*',  // or specific origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}))
 app.use(express.json())
 app.use("/api/tasks",taskApp)
 app.use("/api/users",userRoutes)
