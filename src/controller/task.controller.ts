36def5f5b87450435a792d36f90540ab852ae2e5
Omar <kaliochibrahim@gmail.com>
2026-01-30 20:37:24 +0100

    Fix task retrieval by user ID in getTask function

--- src/controller/task.controller.ts
+++ src/controller/task.controller.ts
@@ -34,7 +34,7 @@ export const addTask = async (req : authRequest, res : Response) => {
 
 export const getTask = async (req : authRequest,res : Response) => {
   if(!req.user) return res.status(403).json({message: "user not authenticated"}) 
-  const tasksDB =  await Task.find(req.user.id)
+  const tasksDB =  await Task.find({userId : req.user.id})
   res.status(200).json(tasksDB)
 }
 
